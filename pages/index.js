3200425c9e1e751a0a811c4430f85b338aa78851
Filipe Borges <filipe.conde@softdesign-rs.com.br>
2019-02-11 11:26:34 -0200

    Initial setup with Bootstrap

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import 'bootstrap/dist/css/bootstrap.css'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+
+const Index = () => (
+  <Container>
+    <Row>
+      <Col>
+        <p className="text-center">Hello World with React/Bootstrap \o/</p>
+      </Col>
+    </Row>
+  </Container>
+)
+
+export default Index
